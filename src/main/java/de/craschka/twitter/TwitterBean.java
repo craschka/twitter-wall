e751839c0fe01ead639ad978d35eb01c82d98926
Christian Raschka <christian.raschka@gmail.com>
2010-10-28 22:09:42 +0200

    added tweets to xhtml

--- src/main/java/de/craschka/twitter/TwitterBean.java
+++ src/main/java/de/craschka/twitter/TwitterBean.java
@@ -3,9 +3,11 @@ package de.craschka.twitter;
 import de.craschka.twitter.api.Tweet;
 import de.craschka.twitter.api.TwitterSearch;
 
+import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 import java.util.List;
 
+@Model
 public class TwitterBean {
     @Inject
     private TwitterSearch twitterSearch;
